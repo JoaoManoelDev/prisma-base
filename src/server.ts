8a3aa9782ec87639f29e7e5cb291931238d0c212
João <joaomanoeldev@gmail.com>
2023-01-17 00:03:28 -0300

    Inserting data into the table

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,20 @@
 import express from 'express'
+import { prisma } from './lib/prisma'
 
 const app = express()
+
+async function main() {
+  const response = await prisma.courses.create({
+    data: {
+      name: 'Curso de NodeJS',
+      duration: 200,
+      description: 'Curso de NodeJs básico.'
+    }
+  })
+
+  console.log(response)
+}
+
+main()
 
 app.listen(3333, () => console.log('Server running in port 3333.'))
